38e77bd9f6a1908fcfe6055090681af46e85a3ad
rudra pramanik <rudrahimangshoo2020@gmail.com>
2023-03-04 03:12:54 +0600

    update

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,10 +1,10 @@
 export const contactsData = {
-  email: 'muradul.1707436@std.hstu.ac.bd',
+  email: 'mkabir.me17@gmail.com',
   phone: '+880 1779796837',
   address: 'Road 8,Block C,Banasree,Rampura,Dhaka',
   Blogger: 'https://xamilmurad.blogspot.com/?m=1',
   facebook: 'https://www.facebook.com/xamil.murad.2017?mibextid=ZbWKwL',
-  linkedIn: 'https://www.linkedin.com/in/m-kabir-8b5b23199',
+  linkedIn: 'https://www.linkedin.com/in/muradul-kabir-8b5b23199',
   twitter: 'https://twitter.com/MuradulKabir3?t=dgDdpFqCfTvcDaegn8iNFA&s=09',
   medium: '',
   stackOverflow: '',
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
   image: 'https://ibb.co/MC4Y0RV',
   imagebw: '',
   resumePdf:
-    'https://drive.google.com/file/d/1x5i5ROMvxNHfBsfaTwZwT2UbPDBlo2g-/view?usp=share_link',
+    'https://drive.google.com/file/d/1csq0wSe0zscKV4-Q2dqPG9ixvO8qz7Vj/view?usp=share_link',
 };
